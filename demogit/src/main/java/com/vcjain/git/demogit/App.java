0539e1ba9fbba763e65e765198b428f36cbef0da
vcjain <vikash.jain@hotmail.com>
2013-04-05 15:17:17 +0530

    Second Commit

--- demogit/src/main/java/com/vcjain/git/demogit/App.java
+++ demogit/src/main/java/com/vcjain/git/demogit/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Commit to BaseProduct :1" );
+        System.out.println( "Commit to BaseProduct :2" );
     }
 }
