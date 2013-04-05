306acc7ed05a8754b685f9244eb144e3eec8e668
vcjain <vikash.jain@hotmail.com>
2013-04-06 00:10:37 +0530

    Fourth commit on Base product

--- demogit/src/main/java/com/vcjain/git/demogit/App.java
+++ demogit/src/main/java/com/vcjain/git/demogit/App.java
@@ -13,6 +13,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Commit to BaseProduct :3" );
+        System.out.println( "Commit to BaseProduct :4" );
     }
 }
